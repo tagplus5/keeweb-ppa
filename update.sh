416ced295d5c4e743086ebddd18d122633a2b1d3
tagplus5 <tagplus5@gmail.com>
2018-02-09 11:26:57 +0200

    update: download only last version and delete old versions except 5

--- update.sh
+++ update.sh
@@ -1,17 +1,15 @@
-#/bin/bash
+#!/bin/bash
 
 BASEDIR=`dirname "$0"` 
 FULLPATH=`cd "$BASEDIR"; pwd`
 cd $FULLPATH
-
-git pull
 
 mkdir -p $FULLPATH/ubuntu
 cd $FULLPATH/ubuntu
 
 COUNT1=`ls -1 | wc -l`
 
-LINKS=`wget -q --output-document - https://github.com/keeweb/keeweb/releases | grep -Eo '/.*deb'`
+LINKS=`wget -q --output-document - https://github.com/keeweb/keeweb/releases | grep -Eo '/.*deb' | head -n 2`
 
 while read -r url; do
     wget -q -N https://github.com$url
@@ -19,6 +17,10 @@ done <<< "$LINKS"
 
 COUNT2=`ls -1 | wc -l`
 
-if [ "$COUNT1" != "$COUNT2" ]; then    
-    bash $FULLPATH/push.sh
-fi;
+if [ "$COUNT1" != "$COUNT2" ]; then
+    ls -F *x64.deb | head -n -5 | xargs rm 2> /dev/null
+    ls -F *ia32.deb | head -n -5 | xargs rm 2> /dev/null
+    echo 1
+else
+    echo 0
+fi
