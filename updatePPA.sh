8ae4288aeb7cd4a9b16fe39ec62ceaca267c5600
tagplus5 <tagplus5@gmail.com>
2018-02-14 09:40:16 +0000

    update

--- updatePPA.sh
+++ updatePPA.sh
@@ -5,7 +5,7 @@ FULLPATH=`cd "$BASEDIR"; pwd`
 
 cd $FULLPATH/ubuntu
 
-dpkg-scanpackages -m . /dev/null > Packages
+dpkg-scanpackages -m . > Packages
 gzip --keep --force -9 Packages
 
 apt-ftparchive release . > Release
