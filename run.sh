5ec54f015401ecf15ddf7dd3677b1ccf875ae085
tagplus5 <tagplus5@gmail.com>
2018-02-14 09:30:53 +0000

    update

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ function pull() {
   docker run --rm \
   -v $FULLPATH/root:/root \
   -v $FULLPATH:/app \
-  tagplus5/ubuntu-ppa git pull -q
+  tagplus5/ubuntu-ppa git pull
 }
 
 function checkVersion() {
